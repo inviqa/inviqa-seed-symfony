b9ee1778cc3e4c2e003ba19a1991962aaeb04db5
Mike Simons <msimons@inviqa.com>
2014-02-17 17:11:32 +0100

    Replace {{name}}.development.local with {{hostname}}

--- tools/capistrano/config/deploy/dev.rb
+++ tools/capistrano/config/deploy/dev.rb
@@ -2,4 +2,4 @@ set :deploy_to, "/tmp/test" # Deploys to VM not presently supported
 set :branch, "develop"
 
 ssh_options[:keys] = [File.join(ENV['HOME'], '.vagrant.d', 'insecure_private_key')]
-server "vagrant@{{name}}.development.local", :app, :primary => true
+server "vagrant@{{hostname}}", :app, :primary => true
