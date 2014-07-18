f77b57af5b25be0915ca50852ea93a47833b1dae
Mike Simons <msimons@inviqa.com>
2014-07-18 12:36:12 +0200

    Integrates data-bag-merge & moves mysql credentials to development env data bag

--- tools/chef/knife.rb
+++ tools/chef/knife.rb
@@ -2,4 +2,5 @@ cookbook_path [ "./site-cookbooks" ]
 role_path "./roles"
 data_bag_path "./data_bags"
 environment_path "./environments"
-#encrypted_data_bag_secret "./secret.key"
+
+# encrypted_data_bag_secret "./encrypted_data_bag_secret"
