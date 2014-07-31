c4a7afd46ee14840f4e3e52047e101d9dd868afb
Mike Simons <msimons@inviqa.com>
2014-07-31 17:29:03 +0200

    Hobo version constraint for SSL functionality

--- seedinit.rb
+++ seedinit.rb
@@ -1,3 +1,8 @@
+if ::Semantic::Version.new(Hobo::VERSION) < ::Semantic::Version.new('0.0.13')
+  FileUtils.rm_rf Hobo.project_config.project_path
+  raise Hobo::UserError.new "This seed requires at least hobo 0.0.13\n\nPlease upgrade with `gem install hobo-inviqa`"
+end
+
 # Overwrite hobo README with project README
 old_readme = File.join(Hobo.project_config.project_path, 'README.md')
 new_readme = File.join(Hobo.project_config.project_path, 'README.project.md')
