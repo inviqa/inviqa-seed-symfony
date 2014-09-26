58bcef1eadc5d5471dbd30fb7c1e517b68a39964
Mike Simons <msimons@inviqa.com>
2014-09-26 17:22:24 +0200

    Adds semantic to seedinit to prevent error if not already loaded by hobo (as might happen with --skip-host-checks)

--- seedinit.rb
+++ seedinit.rb
@@ -1,3 +1,5 @@
+require 'semantic'
+
 if ::Semantic::Version.new(Hobo::VERSION) < ::Semantic::Version.new('0.0.13')
   FileUtils.rm_rf Hobo.project_config.project_path
   raise Hobo::UserError.new "This seed requires at least hobo 0.0.13\n\nPlease upgrade with `gem install hobo-inviqa`"
