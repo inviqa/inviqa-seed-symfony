b3146adf9559452c24e9fd3ff6269c035246f45c
Andy Thompson <me@andytson.com>
2016-02-04 15:49:28 +0000

    Update capistrano dev stage to support Vagrant 1.7+
    Vagrant 1.7 introduced new per-VM private key replacement

--- tools/capistrano/config/deploy/dev.rb
+++ tools/capistrano/config/deploy/dev.rb
@@ -1,5 +1,8 @@
 set :deploy_to, "/tmp/test" # Deploys to VM not presently supported
 set :branch, "develop"
 
-ssh_options[:keys] = [File.join(ENV['HOME'], '.vagrant.d', 'insecure_private_key')]
+ssh_options[:keys] = [
+  File.expand_path(File.join('..', '..', '..', 'vagrant', '.vagrant', 'machines', 'default', 'virtualbox', 'private_key'), __dir__),
+  File.join(ENV['HOME'], '.vagrant.d', 'insecure_private_key')
+].select { |file| File.exist?(file) }.take(1)
 server "vagrant@{{hostname}}", :app, :primary => true
