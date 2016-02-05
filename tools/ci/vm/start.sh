01d66d12d2cab933e2713948d5dbe281ffb5ec7e
Andy Thompson <athompson@inviqa.com>
2016-02-05 17:02:28 +0000

    Update ci vm scripts to use Hem

--- tools/ci/vm/start.sh
+++ tools/ci/vm/start.sh
@@ -1,16 +1,12 @@
-#!/bin/bash
+#!/bin/bash -eux
 source $(dirname $0)/shared.sh
-
-# Clone Hobo gemset to project gemset
-clone_gemset $RUBY_VERSION $HOBO_GEMSET_NAME $GEMSET_NAME
-use_gemset $RUBY_VERSION $GEMSET_NAME
 
 # Jenkins creates the workspace as "nobody" (gid -2 on OSX) which is problematic for Vagrant
 sudo chgrp wheel `pwd`
 
 # Rebuild to VM unless explicitly instructed not to
 if [[ $NO_REBUILD == 1 ]]; then
-  hobo vm start
+  hem vm up
 else
-  hobo vm rebuild
+  hem vm rebuild
 fi
--- tools/ci/vm/test.sh
+++ tools/ci/vm/test.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/bin/bash -eux
 source $(dirname $0)/shared.sh
 
-use_gemset $RUBY_VERSION $GEMSET_NAME
 cd tools/vagrant
-rake
+bundle exec rake
