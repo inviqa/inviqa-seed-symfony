39da2b86eb4acbfc65b82e74ef09a69161c374a3
Andy Thompson <me@andytson.com>
2016-02-11 12:56:05 +0000

    Run composer directly on the capistrano release
    Composer introduced ~/.composer/cache/files/ to store downloaded packages so reinstalls will use it as cache.
    
    This means the capistrano composer tasks don't need to keep their own cache directory, and can instead rely on the built-in cache.

--- tools/capistrano/lib/inviqa_cap/composer.rb
+++ tools/capistrano/lib/inviqa_cap/composer.rb
@@ -10,15 +10,12 @@ module InviqaCap
         namespace :inviqa do
           namespace :composer do
             task :setup do
-              run "mkdir -p #{shared_path}/composer"
               run "cd #{shared_path} && curl -sS https://getcomposer.org/installer | php"
             end
 
             task :install do
-              run "cp #{latest_release}/composer.* #{shared_path}/composer"
               run "cd #{shared_path} && php #{shared_path}/composer.phar self-update"
-              run "cd #{shared_path}/composer && php #{shared_path}/composer.phar install --no-dev --optimize-autoloader"
-              run "cp -R #{shared_path}/composer/* #{latest_release}"
+              run "cd #{latest_release} && php #{shared_path}/composer.phar install --no-dev --optimize-autoloader"
             end
           end
         end
