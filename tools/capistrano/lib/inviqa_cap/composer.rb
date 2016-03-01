415404986df04fc4325815b464a27f82fdd779a8
Andy Thompson <me@andytson.com>
2016-03-01 15:06:14 +0000

    Update composer to use custom composer home directory
    Capistrano with deploy user sudo will use the pre-sudo HOME path, causing multiple users to have their own cache for the same application.
    
    This isn't wanted, and though sudo -i would solve this, we want to lock down the deploy user, which stops that from working.

--- tools/capistrano/lib/inviqa_cap/composer.rb
+++ tools/capistrano/lib/inviqa_cap/composer.rb
@@ -4,18 +4,19 @@ module InviqaCap
   class Composer
     def self.load_into(config)
       config.load do
+        _cset(:composer_home) { "#{shared_path}/composer" }
         after "deploy:setup", "inviqa:composer:setup"
         after "deploy:finalize_update", "inviqa:composer:install"
 
         namespace :inviqa do
           namespace :composer do
             task :setup do
-              run "cd #{shared_path} && curl -sS https://getcomposer.org/installer | php"
+              run "cd #{shared_path} && curl -sS https://getcomposer.org/installer | /usr/bin/env COMPOSER_HOME=#{composer_home} php"
             end
 
             task :install do
-              run "cd #{shared_path} && php #{shared_path}/composer.phar self-update"
-              run "cd #{latest_release} && php #{shared_path}/composer.phar install --no-dev --optimize-autoloader"
+              run "cd #{shared_path} && COMPOSER_HOME=#{composer_home} php #{shared_path}/composer.phar self-update"
+              run "cd #{latest_release} && COMPOSER_HOME=#{composer_home} php #{shared_path}/composer.phar install --no-dev --optimize-autoloader"
             end
           end
         end
