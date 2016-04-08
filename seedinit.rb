7e1a5a5e1987ad133318882efa7a51d8cce97475
Andy Thompson <athompson@inviqa.com>
2016-04-08 17:17:14 +0100

    Update seed Hem version requirements

--- seedinit.rb
+++ seedinit.rb
@@ -1,9 +1,5 @@
-require 'semantic'
 
-unless defined?(Hem) && ::Semantic::Version.new(Hem::VERSION).satisfies('1.0.1-beta6')
-  FileUtils.rm_rf Hobo.project_config.project_path
-  raise Hobo::UserError.new "This seed requires at least hem 1.0.1-beta6\n\nPlease upgrade at https://github.com/inviqa/hem"
-end
+Hem.require_version '>= 1.1.0'
 
 # Overwrite hem README with project README
 old_readme = File.join(Hem.project_config.project_path, 'README.md')
