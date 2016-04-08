0920619c9f5714b10afe8e29ae78727e51df7d89
Andy Thompson <athompson@inviqa.com>
2016-04-08 17:24:20 +0100

    Remove the seed readme, and change the project readme to it's erb template

--- seedinit.rb
+++ seedinit.rb
@@ -3,5 +3,6 @@ Hem.require_version '>= 1.1.0'
 
 # Overwrite hem README with project README
 old_readme = File.join(Hem.project_config.project_path, 'README.md')
-new_readme = File.join(Hem.project_config.project_path, 'README.project.md')
-FileUtils.mv new_readme, old_readme
+new_readme = File.join(Hem.project_config.project_path, 'README.project.md.erb')
+File.delete old_readme
+FileUtils.mv new_readme, "#{old_readme}.erb"
