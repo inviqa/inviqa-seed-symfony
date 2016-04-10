4ba478da9a3c21e54f5f0c1e7be48b446eeb1b0a
Andy Thompson <athompson@inviqa.com>
2016-04-10 12:10:58 +0100

    Generate random mysql passwords

--- seedinit.rb
+++ seedinit.rb
@@ -1,3 +1,4 @@
+require 'securerandom'
 
 Hem.require_version '>= 1.1.0'
 
@@ -7,4 +8,10 @@ new_readme = File.join(Hem.project_config.project_path, 'README.project.md.erb')
 File.delete old_readme
 FileUtils.mv new_readme, "#{old_readme}.erb"
 
+# Project local VM SSL certificate
 Hem.project_config.tmp.chef_ssl = Hem::Lib::SelfSignedCertGenerator.generate(Hem.project_config.hostname)
+
+# MySQL local VM MySQL passwords
+Hem.project_config.tmp.mysql_root_password = SecureRandom.urlsafe_base64(32)
+Hem.project_config.tmp.mysql_repl_password = SecureRandom.urlsafe_base64(32)
+Hem.project_config.tmp.mysql_debian_password = SecureRandom.urlsafe_base64(32)
