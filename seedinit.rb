47292490c397c618e86cdcd7f2b8fbcad7bd8a83
Andy Thompson <athompson@inviqa.com>
2016-04-10 12:01:55 +0100

    Move SSL certificate generation to seedinit
    This was originally done in Hem itself

--- seedinit.rb
+++ seedinit.rb
@@ -6,3 +6,5 @@ old_readme = File.join(Hem.project_config.project_path, 'README.md')
 new_readme = File.join(Hem.project_config.project_path, 'README.project.md.erb')
 File.delete old_readme
 FileUtils.mv new_readme, "#{old_readme}.erb"
+
+Hem.project_config.tmp.chef_ssl = Hem::Lib::SelfSignedCertGenerator.generate(Hem.project_config.hostname)
