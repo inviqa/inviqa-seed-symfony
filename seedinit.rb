30c435c7837f3d69e71f683000b7f82313702e36
Andy Thompson <athompson@inviqa.com>
2016-06-16 15:12:01 +0100

    Use the Hem 1.2 shortened config in seedinit

--- seedinit.rb
+++ seedinit.rb
@@ -1,4 +1,4 @@
-Hem.require_version '>= 1.1.0'
+Hem.require_version '>= 1.2.0'
 
 # Overwrite hem README with project README
 old_readme = 'README.md'
@@ -7,10 +7,10 @@ File.delete old_readme
 FileUtils.mv new_readme, "#{old_readme}.erb"
 
 # Project local VM SSL certificate
-Hem.project_config.tmp.chef_ssl = Hem::Lib::SelfSignedCertGenerator.generate(Hem.project_config.hostname)
+config.tmp.chef_ssl = Hem::Lib::SelfSignedCertGenerator.generate(config.hostname)
 
 # MySQL local VM MySQL passwords
 password = '984C42CF342f7j6' # password still is set in the basebox
-Hem.project_config.tmp.mysql_root_password = password
-Hem.project_config.tmp.mysql_repl_password = password
-Hem.project_config.tmp.mysql_debian_password = password
+config.tmp.mysql_root_password = password
+config.tmp.mysql_repl_password = password
+config.tmp.mysql_debian_password = password
