4243f2efd1d6207bd72383816308e247f9ed1cfa
Kieren Evans <kevans@inviqa.com>
2017-08-09 14:02:19 +0100

    Add more continuouspipe/docker config + add instructions.

--- plan.sh
+++ plan.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
 # plan.sh - used by the base image to insert/replace functions into the `container` tasks
+
+if [ "$(type -t "do_setup")" != "function" ]; then
+  do_setup() {
+    :
+  }
+fi
 
 do_migrate() {
   :
