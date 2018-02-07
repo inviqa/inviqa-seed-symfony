d81a36d6fe33cf5eb62bb995bebf4e745660ad39
Andy Thompson <athompson@inviqa.com>
2018-02-07 11:42:17 +0000

    DEVOPS-556: remove user from Capistrano deploy stages, to inherit from default settings
    Also remove the gateway as it may confuse those who use a bastion configured in .ssh/config ProxyCommand

--- tools/capistrano/config/deploy/production.rb
+++ tools/capistrano/config/deploy/production.rb
@@ -2,5 +2,4 @@ set :deploy_to, "/var/www/" + "domain"
 set :branch, "master"
 set :keep_releases, 5
 
-server "user@domain", :app, :primary => true
-# set :gateway, "user@host" # Use if you need to bounce through another server
+server "domain", :app, :primary => true
--- tools/capistrano/config/deploy/uat.rb
+++ tools/capistrano/config/deploy/uat.rb
@@ -2,5 +2,4 @@ set :deploy_to, "/var/www/" + "uat_domain"
 set :branch, "develop"
 set :keep_releases, 5
 
-server "user@uat_domain", :app, :primary => true
-# set :gateway, "user@host" # Use if you need to bounce through another server
+server "uat_domain", :app, :primary => true
